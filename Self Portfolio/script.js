57aa55f30c40412a891da88e1afd6d3e8a3241b8
Vishakha Gaikwad <126825212+Vishakha221299@users.noreply.github.com>
2024-01-11 03:36:10 +0530

    Add files via upload

---
+++ Self Portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.multiple-text', {
+    strings: ['FrontEnd Developer,', '&amp; A Student,'],
+    typeSpeed: 50,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
